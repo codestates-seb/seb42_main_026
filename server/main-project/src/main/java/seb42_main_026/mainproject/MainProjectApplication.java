77acd1511ac271af5c4198917d2a2fa548e240c6
jiilEun <dmswldlf2010@gmail.com>
2023-03-07 13:46:02 +0900

    Init: 초기 개발환경 구축

---
+++ server/main-project/src/main/java/seb42_main_026/mainproject/MainProjectApplication.java
@@ -0,0 +1,13 @@
+package seb42_main_026.mainproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainProjectApplication.class, args);
+	}
+
+}
---
+++ server/main-project/src/test/java/seb42_main_026/mainproject/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package seb42_main_026.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
