f088b9ebe0c8089b98b47776d85c05c715329f3f
jbsim999 <116049579+jbsim999@users.noreply.github.com>
2023-03-07 19:33:08 +0900

    feat: Global Dto

--- server/main-project/src/main/java/seb42_main_026/mainproject/dto/SingleResponseDto.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/dto/SingleResponseDto.java
@@ -1,2 +1,10 @@
-package seb42_main_026.mainproject.dto;public class SingleResponse {
+package seb42_main_026.mainproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
 }
