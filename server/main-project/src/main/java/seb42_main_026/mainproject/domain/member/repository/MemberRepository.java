c3405f49e8a2ede8fc8e035ac644956bc3f9f528
NakChoi <nakchoi94@gmail.com>
2023-03-08 15:06:37 +0900

    feat: 멤버 구현

---
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package seb42_main_026.mainproject.domain.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import seb42_main_026.mainproject.domain.member.entity.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByNickname(String Nickname);
+}
