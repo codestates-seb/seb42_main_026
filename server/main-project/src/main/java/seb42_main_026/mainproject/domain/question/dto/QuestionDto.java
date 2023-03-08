1c59dc934fb124183a1985a395b7b4edfb2605df
jiilEun <dmswldlf2010@gmail.com>
2023-03-08 15:50:04 +0900

    Feat: 질문 작성 기능 구현

--- server/main-project/src/main/java/seb42_main_026/mainproject/domain/question/dto/QuestionDto.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/question/dto/QuestionDto.java
@@ -2,11 +2,20 @@ package seb42_main_026.mainproject.domain.question.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 
 public class QuestionDto {
+    @Getter
+    @Setter
     public static class Post {
-        private Long memberId;
+//        @Positive
+//        private Long memberId;
+        @NotBlank(message = "제목은 필수로 작성해야 합니다.")
         private String title;
+        @NotBlank(message = "내용은 필수로 작성해야 합니다.")
         private String content;
     }
 
@@ -18,6 +27,7 @@ public class QuestionDto {
 
     @AllArgsConstructor
     @Getter
+    @Setter
     public static class Response {
         private Long questionId;
         private String title;
