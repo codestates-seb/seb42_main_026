1ce544c04d25dd8ee0e3f49e4e5992d32bca4a4f
jiilEun <dmswldlf2010@gmail.com>
2023-03-09 21:30:17 +0900

    Feat: Like 엔티티 클래스 구현

---
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/like/Like.java
@@ -0,0 +1,30 @@
+package seb42_main_026.mainproject.domain.like;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import seb42_main_026.mainproject.domain.member.entity.Member;
+import seb42_main_026.mainproject.domain.question.entity.Question;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "LIKES")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @Column(nullable = false)
+    private int likeCount;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+}
