cfa871e2c7fa0ee2084c6a9506ac7396ea6e3916
jbsim999 <116049579+jbsim999@users.noreply.github.com>
2023-03-10 14:40:33 +0900

    Comment 등록 기능 #33

--- server/main-project/src/main/java/seb42_main_026/mainproject/domain/comment/repository/CommentRepository.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/comment/repository/CommentRepository.java
@@ -1,2 +1,7 @@
-package seb42_main_026.mainproject.domain.comment.repository;public interface CommentRepository {
+package seb42_main_026.mainproject.domain.comment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import seb42_main_026.mainproject.domain.comment.entity.Comment;
+
+public interface CommentRepository extends JpaRepository <Comment, Long> {
 }
