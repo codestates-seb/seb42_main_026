91b86a07ff9e4a9f65818f8a4f4b82ccbaf13a40
jetty8013 <jetty8013@gmail.com>
2023-03-13 04:08:20 +0900

    feat : TopNav 뒤로가기, 상태에따라 라우팅 기능 구현

--- client/src/hooks/usePage.ts
+++ client/src/hooks/usePage.ts
@@ -1,17 +1,18 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { RootState } from "../store/store";
-import { setPage } from "../store/actions";
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { RootState } from '../store/store';
+import { setPage } from '../store/actions';
 
 export function usePage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isPages = useSelector((state: RootState) => state.page.currentPage);
+  const getPageHandler = useSelector((state: RootState) => state.page.currentPage);
 
-  const setPageHandler = () => {
-    dispatch(setPage("home"));
-    navigate("/");
+  const setPageHandler = (isLoggedIn: boolean, id: string, link: string, security: boolean) => {
+    console.log(id);
+    dispatch(isLoggedIn ? setPage(id) : setPage('login'));
+    navigate(isLoggedIn ? link : security ? '/Login' : link); //추후 navigate 수정 page 이동시마다 login 검증 필요
   };
 
-  return { setPageHandler, isPages };
+  return { setPageHandler, getPageHandler };
 }
