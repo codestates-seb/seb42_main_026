c479e9711053e148ccc02da3c2344c5d309a0fb3
jetty8013 <jetty8013@gmail.com>
2023-03-12 15:04:22 +0900

    feat : ButtonStyled 가로,세로 옵션 추가

--- client/src/components/ButtonStyled.tsx
+++ client/src/components/ButtonStyled.tsx
@@ -1,33 +1,35 @@
 import styled from "styled-components";
 
-
 interface ButtonProps {
   title?: string;
-  color?: 'pink' | 'normal';
+  color?: "pink" | "normal";
+  width?: string;
+  height?: string;
 }
 
-const Button = ({ color, title }: ButtonProps) => {
+const Button = ({ color, title, width = '', height = '' }: ButtonProps) => {
   return (
-    <>
-      <LoginButton color={color}>
-        <LoginText>{title}</LoginText>
-      </LoginButton>
-    </>
+    <LoginButton color={color} width={width} height={height}>
+      <LoginText>{title}</LoginText>
+    </LoginButton>
   );
 };
 
 export default Button;
 
 const LoginButton = styled.button<ButtonProps>`
-  height: 55px;
-  align-self: stretch;
+  width: ${(props) => props.width === ''? '100%' : props.width};
+  height: ${(props) => props.height === ''? '55px' : props.height};
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 5px;
-  color: ${(props) => props.color === "pink"  ? '#fff' : props.theme.colors.gray01};
-  border: ${(props) => props.color === "pink"  ? 'none' : `solid 1px ${props.theme.colors.gray03}`};
-  background-color: ${(props) => props.color === "pink" ? props.theme.colors.mobMain : '#fff'};
+  color: ${(props) =>
+    props.color === "pink" ? "#fff" : props.theme.colors.gray01};
+  border: ${(props) =>
+    props.color === "pink" ? "none" : `solid 1px ${props.theme.colors.gray03}`};
+  background-color: ${(props) =>
+    props.color === "pink" ? props.theme.colors.mobMain : "#fff"};
 `;
 const LoginText = styled.span<ButtonProps>`
   font-family: "Noto Sans KR";
