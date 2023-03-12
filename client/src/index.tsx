6726eeabfb5ceae16e3f56895291e6e1411843d0
ssomni <ssm9991@gmail.com>
2023-03-12 19:17:12 +0900

    design: 글로벌 스타일 수정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { style } from "./style/global";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const { GlobalStyle, theme } = style();
+const { GlobalStyle } = style();
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
+    <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
