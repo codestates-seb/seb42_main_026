d912ef03ac3938b49c2b1e0dd6f017ac66cc86d7
jetty8013 <jetty8013@gmail.com>
2023-03-13 14:04:34 +0900

    feat : buttonClickHandler 추가

--- client/src/components/ButtonStyled.tsx
+++ client/src/components/ButtonStyled.tsx
@@ -1,22 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 interface ButtonProps {
   title?: string;
-  color?: "pink" | "normal";
+  color?: 'pink' | 'normal';
   width?: string;
   height?: string;
   radius?: string;
+  buttonClickHandler?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({
-  color,
-  title,
-  width = "",
-  height = "",
-  radius = "",
-}: ButtonProps) => {
+const Button = ({ color, title, width = '', height = '', radius = '', buttonClickHandler }: ButtonProps) => {
   return (
-    <LoginButton color={color} width={width} height={height} radius={radius}>
+    <LoginButton onClick={buttonClickHandler} color={color} width={width} height={height} radius={radius}>
       <LoginText>{title}</LoginText>
     </LoginButton>
   );
@@ -25,21 +20,18 @@ const Button = ({
 export default Button;
 
 const LoginButton = styled.button<ButtonProps>`
-  width: ${(props) => (props.width === "" ? "100%" : props.width)};
-  height: ${(props) => (props.height === "" ? "55px" : props.height)};
+  width: ${(props) => (props.width === '' ? '100%' : props.width)};
+  height: ${(props) => (props.height === '' ? '55px' : props.height)};
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: ${(props) => (props.radius === "" ? "5px" : props.radius)};
-  color: ${(props) =>
-    props.color === "pink" ? `var(--color-white01)` : `var(--color-gray01)`};
-  border: ${(props) =>
-    props.color === "pink" ? "none" : `solid 1px var(--color-gray03)`};
-  background-color: ${(props) =>
-    props.color === "pink" ? `var(--color-mobMain)` : `var(--color-white01)`};
+  border-radius: ${(props) => (props.radius === '' ? '5px' : props.radius)};
+  color: ${(props) => (props.color === 'pink' ? `var(--color-white01)` : `var(--color-gray01)`)};
+  border: ${(props) => (props.color === 'pink' ? 'none' : `solid 1px var(--color-gray03)`)};
+  background-color: ${(props) => (props.color === 'pink' ? `var(--color-mobMain)` : `var(--color-white01)`)};
 `;
 const LoginText = styled.span`
-  font-family: "Noto Sans KR";
+  font-family: 'Noto Sans KR';
   font-size: var(--font-size14);
   letter-spacing: var(--font-spacing-title);
 `;
