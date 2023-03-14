d7781d80e1b0ee4389bad1c81230efbccf2ce837
jbsim999 <116049579+jbsim999@users.noreply.github.com>
2023-03-14 11:33:53 +0900

    feat: StorageException 추가

--- server/main-project/src/main/java/seb42_main_026/mainproject/exception/StorageException.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/exception/StorageException.java
@@ -1,2 +1,10 @@
-package seb42_main_026.mainproject.exception;public class StorageException {
+package seb42_main_026.mainproject.exception;
+
+public class StorageException extends RuntimeException{
+
+    public StorageException(String message){ super(message);}
+
+    public StorageException(String message, Throwable cause){
+        super(message, cause);
+    }
 }
