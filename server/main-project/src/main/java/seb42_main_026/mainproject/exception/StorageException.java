4f50569aecbd85e339b17da592c8308bd513bf37
jbsim999 <116049579+jbsim999@users.noreply.github.com>
2023-03-14 11:33:53 +0900

    feat: StorageException 추가

--- server/main-project/src/main/java/seb42_main_026/mainproject/exception/StorageException.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/exception/StorageException.java
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d7781d8 (feat: StorageException 추가)
 package seb42_main_026.mainproject.exception;
 
 public class StorageException extends RuntimeException{
@@ -8,7 +11,10 @@ public class StorageException extends RuntimeException{
     public StorageException(String message, Throwable cause){
         super(message, cause);
     }
+<<<<<<< HEAD
 =======
 package seb42_main_026.mainproject.exception;public class StorageException {
 >>>>>>> b732129 (feat: MediaFile Upload)
+=======
+>>>>>>> d7781d8 (feat: StorageException 추가)
 }
