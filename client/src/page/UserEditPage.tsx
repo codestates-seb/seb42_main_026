42c94fc88e225da5a2eb167cbb9f938e54b21056
jetty8013 <jetty8013@gmail.com>
2023-03-14 13:00:25 +0900

    rename : UserEditPage 내부 스타일 컴포넌트 이름 변경

--- client/src/page/UserEditPage.tsx
+++ client/src/page/UserEditPage.tsx
@@ -3,73 +3,41 @@ import { Link } from 'react-router-dom';
 
 const UserEditPage = () => {
   return (
-    <SignupWrapper>
-      <Title>PPONG</Title>
+    <UserEditPageWrapper>
       <InputContainer>
         <InputText>닉네임</InputText>
-        <SignupInput placeholder="변경할 닉네임을 입력해주세요."></SignupInput>
+        <UserEditInput placeholder="변경할 닉네임을 입력해주세요."></UserEditInput>
         <InputText>현재 비밀번호</InputText>
-        <SignupInput placeholder="현재 비밀번호를 입력해주세요."></SignupInput>
+        <UserEditInput placeholder="현재 비밀번호를 입력해주세요."></UserEditInput>
         <InputText>새 비밀번호</InputText>
-        <SignupInput placeholder="변경할 비밀번호를 입력해주세요."></SignupInput>
+        <UserEditInput placeholder="변경할 비밀번호를 입력해주세요."></UserEditInput>
         <InputText>새 비밀번호 확인</InputText>
-        <SignupInput placeholder="변경할 비밀번호를 한 번 더 입력해주세요."></SignupInput>
-        <SignupButton>
-          <SignupButtonText>수정하기</SignupButtonText>
-        </SignupButton>
+        <UserEditInput placeholder="변경할 비밀번호를 한 번 더 입력해주세요."></UserEditInput>
+        <UserEditButton>
+          <UserEditButtonText>수정하기</UserEditButtonText>
+        </UserEditButton>
       </InputContainer>
-      <ContourContainer>
-        <ContourLine></ContourLine>
-        <ContourText>또는</ContourText>
-      </ContourContainer>
-      <SignupText>
-        <Link to="/login">탈퇴</Link>
-      </SignupText>
-    </SignupWrapper>
+      <UserEditText>
+        {/* 탈퇴페이지로 수정해야함 */}
+        <Link to="/RemoveAccount">탈퇴</Link>
+      </UserEditText>
+    </UserEditPageWrapper>
   );
 };
 
-const SignupText = styled.span`
-  padding-top: 20px;
+const UserEditText = styled.span`
   font-family: 'Noto Sans KR';
   font-size: 14px;
   color: #878b93;
   text-align: center;
   letter-spacing: -0.05em;
   a {
-    color: #ff607c;
+    color: var(--color-gray03);
     font-weight: 500;
   }
 `;
 
-const ContourContainer = styled.div`
-  position: relative;
-  display: flex;
-  align-self: stretch;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 0 30px;
-`;
-
-const ContourLine = styled.div`
-  align-self: stretch;
-  border: 0.5px solid;
-  border-color: #abaeb4;
-`;
-const ContourText = styled.span`
-  position: absolute;
-  width: 62px;
-  text-align: center;
-  letter-spacing: -0.05em;
-  background-color: #ffffff;
-  font-family: 'Noto Sans KR';
-  font-weight: 400;
-  font-size: 14px;
-  color: #abaeb4;
-`;
-
-const SignupWrapper = styled.div`
+const UserEditPageWrapper = styled.div`
   user-select: none;
   align-self: stretch;
   height: 844px;
@@ -81,7 +49,7 @@ const SignupWrapper = styled.div`
   gap: 10px;
 `;
 
-const SignupButton = styled.button`
+const UserEditButton = styled.button`
   margin-top: 12px;
   height: 55px;
   align-self: stretch;
@@ -92,23 +60,13 @@ const SignupButton = styled.button`
   border: none;
   background-color: #ff607c;
 `;
-const SignupButtonText = styled.span`
+const UserEditButtonText = styled.span`
   font-family: 'Noto Sans KR';
   font-size: 14px;
   color: #ffffff;
   letter-spacing: -0.05em;
 `;
-
-const Title = styled.span`
-  font-family: 'Roboto';
-  color: #ff607c;
-  text-align: center;
-  font-weight: 900;
-  font-size: 40px;
-  letter-spacing: -0.05em;
-`;
-
-const SignupInput = styled.input`
+const UserEditInput = styled.input`
   /* Frame 23 */
   align-self: stretch;
   height: 48px;
@@ -118,7 +76,6 @@ const SignupInput = styled.input`
   border-radius: 5px;
   padding-left: 8px;
 `;
-
 const InputText = styled.span`
   font-family: 'Noto Sans KR';
   font-weight: 400;
