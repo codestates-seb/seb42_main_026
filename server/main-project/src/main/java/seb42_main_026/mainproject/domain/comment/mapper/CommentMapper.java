646a27883868ac0cfbc85b19252a89d59aca12a7
jbsim999 <116049579+jbsim999@users.noreply.github.com>
2023-03-15 15:20:43 +0900

    Feat: 조회 기능 #9

--- server/main-project/src/main/java/seb42_main_026/mainproject/domain/comment/mapper/CommentMapper.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/comment/mapper/CommentMapper.java
@@ -1,14 +1,31 @@
 package seb42_main_026.mainproject.domain.comment.mapper;
 
 import org.mapstruct.Mapper;
+import seb42_main_026.mainproject.domain.answer.entity.Answer;
 import seb42_main_026.mainproject.domain.comment.dto.CommentDto;
 import seb42_main_026.mainproject.domain.comment.entity.Comment;
+import seb42_main_026.mainproject.domain.member.entity.Member;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
-    Comment commentPostDtoToComment(CommentDto.Post commentPostDto);
+//    Comment commentPostDtoToComment(CommentDto.Post commentPostDto);
+    default Comment commentPostDtoToComment(CommentDto.Post commentPostDto){
+        Comment comment = new Comment();
+
+        Member member = new Member();
+        member.setMemberId(commentPostDto.getMemberId());
+
+        Answer answer = new Answer();
+        answer.setAnswerId(commentPostDto.getAnswerId());
+
+        comment.setMember(member);
+        comment.setAnswer(answer);
+        comment.setContent(commentPostDto.getContent());
+
+        return comment;
+    }
 
     Comment commentPatchDtoToComment(CommentDto.Patch commentPatchDto);
 
