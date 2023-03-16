e7468ba4e80ed21a84e3cad28794c8c46f5dba7a
NakChoi <nakchoi94@gmail.com>
2023-03-16 18:19:06 +0900

    Fix: MemberDto Validation 수정

--- server/main-project/src/main/java/seb42_main_026/mainproject/domain/member/dto/MemberDto.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/member/dto/MemberDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -13,14 +14,14 @@ public class MemberDto {
     public static class Post{
 
         @NotBlank
-        @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "유효한 이메일이 아닙니다." )
+        @Email
         private String email;
 
         @NotBlank
         @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$", message = "비밀번호는 영문과 특수문자 숫자를 포함하여, 8자 이상 20자 이하여야 합니다." )
         private String password;
 
-        @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,10}$", message = "별명은 2자 이상 10자 이하, 영어 또는 한글 또는 숫자로 구성되어야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,10}$", message = "별명은 2자 이상 10자 이하, 영어 또는 한글 또는 숫자로 구성되어야 합니다.")
         private String nickname;
 
     }
@@ -30,7 +31,7 @@ public class MemberDto {
         @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$", message = "비밀번호는 영문과 특수문자 숫자를 포함하여, 8자 이상 20자 이하여야 합니다." )
         private String password;
 
-        @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,10}$", message = "별명은 2자 이상 10자 이하, 영어 또는 한글 또는 숫자로 구성되어야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,10}$", message = "별명은 2자 이상 10자 이하, 영어 또는 한글 또는 숫자로 구성되어야 합니다.")
         private String nickname;
     }
 
