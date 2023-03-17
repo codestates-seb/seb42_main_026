0ea574e1103cfc3a14e60509538de2a910a7e914
jinaSE0 <tiffany6771@gmail.com>
2023-03-17 13:45:10 +0900

    feat: 홈페이지 게시글 get 요청 및 날짜 형식 수정

--- client/src/page/HomePage.tsx
+++ client/src/page/HomePage.tsx
@@ -1,53 +1,66 @@
-import styled from 'styled-components';
-import axios from 'axios';
-import SubApp from '../container/home/SubApp';
-import BoardItem from '../container/naggingboard/BoardItem';
+import styled from "styled-components";
+import axios from "axios";
+import SubApp from "../container/home/SubApp";
+import BoardItem from "../container/naggingboard/BoardItem";
+import { useEffect, useState } from "react";
+
+export interface IQuestion {
+  title: string;
+  nickname: string;
+  likeCount: number;
+  createdAt: Date;
+  answerCount: number;
+}
 
 export default function HomePage() {
-  const dummyData = [
-    { title: '잔소리 오지게 해줄 사람', nickname: '황금올리브닭다리', likeCount: 220, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '고양킹', likeCount: 100, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '강아지는귀여워', likeCount: 660, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '자르반 4세', likeCount: 603, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '피그마로 밥먹는 사람', likeCount: 610, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '백엔드의 황제', likeCount: 500, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '당신누구요', likeCount: 620, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '연진아너X됐어', likeCount: 699, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '리액트가뭔가요', likeCount: 601, createdAt: '03/07', answerCount: 20 },
-    { title: '잔소리 오지게 해줄 사람', nickname: '코드로 밥먹기', likeCount: 654, createdAt: '03/07', answerCount: 20 },
-    // 동점이면 날짜순으로 하는 코드 작성해야함 백엔드와 상의
-  ];
-  async function boardquestions() {
-    const params = {
-      page: 1,
-      size: 20,
-    };
-    try {
-      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/board/questions`, { params });
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  const [list, setList] = useState([]);
+
+  const parseDate = (props: Date) => {
+    const now = new Date(props);
+    const MM =
+      Number(now.getMonth()) < 10 ? `0${now.getMonth()}` : now.getMonth();
+    const dd = Number(now.getDate()) < 10 ? `0${now.getDate()}` : now.getDate();
+    return `${MM}/${dd}`;
+  };
 
   async function homequestions() {
     try {
-      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/home/questions`);
-      console.log(response);
+      const response = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/home/questions`
+      );
+      if (response?.data.data.length) {
+        setList(response?.data.data);
+      }
     } catch (error) {
       console.log(error);
     }
   }
+  console.log(list);
+
+  useEffect(() => {
+    homequestions();
+  }, []);
 
   return (
     <HomePageWrapper>
       <SubApp />
       <PopulerBoardTitle>인기 잔소리</PopulerBoardTitle>
-      <button onClick={() => homequestions()}>test</button>
       <PopulerBoard>
-        {dummyData.map(({ title, nickname, likeCount, createdAt, answerCount }, index) => (
-          <BoardItem key={index} title={title} likeCount={likeCount} nickname={nickname} createdAt={createdAt} answerCount={answerCount} />
-        ))}
+        {list.map(
+          (
+            { title, nickname, likeCount, createdAt, answerCount }: IQuestion,
+            index: number
+          ) => (
+            <BoardItem
+              key={index}
+              title={title}
+              likeCount={likeCount}
+              nickname={nickname}
+              createdAt={parseDate(createdAt)}
+              answerCount={answerCount}
+            />
+          )
+        )}
       </PopulerBoard>
     </HomePageWrapper>
   );
