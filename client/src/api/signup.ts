40549a4e90f18aa14172dde37cae0e426e8d63f6
jetty8013 <jetty8013@gmail.com>
2023-03-17 16:16:50 +0900

    feat : Signup Api Error 메세지 처리

--- client/src/api/signup.ts
+++ client/src/api/signup.ts
@@ -1,17 +1,29 @@
 import axios from 'axios';
 
+interface CustomError extends Error {
+  response?: {
+    data: any;
+    status: number;
+  };
+}
+
 const signup = async (email: string, password: string, nickname: string) => {
   try {
-    const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/members/signup`, {
+    const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/signup`, {
       email,
       password,
       nickname,
     });
     const { data } = response;
     return data; // 서버에서 발급한 토큰 등의 정보가 담긴 객체
-  } catch (error) {
-    console.error(error);
-    return null;
+  } catch (error: unknown) {
+    const customErr = error as CustomError;
+    console.log(customErr.response?.data.fieldErrors[0].field);
+    console.log(customErr.response?.data.fieldErrors[0].message);
+    return {
+      field: customErr.response?.data.fieldErrors[0].field,
+      message: customErr.response?.data.fieldErrors[0].message,
+    };
   }
 };
 
