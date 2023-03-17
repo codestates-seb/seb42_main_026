1d2a7401d74468baf35938b6b25d0585b52685dd
ssomni <ssm9991@gmail.com>
2023-03-17 23:51:52 +0900

    feat: 쿠키에 저장한 토큰 꺼내서 요청해더에 넣어주기

--- client/src/api/apis.ts
+++ client/src/api/apis.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+function getCookie(key: string | RegExp | undefined) {
+  key = new RegExp(key + "=([^;]*)"); // 쿠키들을 세미콘론으로 구분하는 정규표현식 정의
+  return key.test(document.cookie) ? unescape(RegExp.$1) : ""; // 인자로 받은 키에 해당하는 키가 있으면 값을 반환
+}
+
 const customAxios = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    Authorization: "JWT TOKEN",
+    Authorization: getCookie("accessToken"),
   },
   //   timeout: 1000,
 });
@@ -11,7 +16,7 @@ const customAxios = axios.create({
 export default customAxios;
 
 // 요청에 쿠키, 인증헤더,인증서와 같은 자격증명을 포함해야하는지 여부를 나타내는 데 사용
-customAxios.defaults.withCredentials = true;
+// customAxios.defaults.withCredentials = true;
 
 // GET 요청 예시
 // customAxios
