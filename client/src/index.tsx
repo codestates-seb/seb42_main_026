11293793026fe71cfc7ba26aebf98d79e7966aa1
jetty8013 <jetty8013@gmail.com>
2023-03-18 23:37:01 +0900

    fix : stricmode 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { style } from "./style/global";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { style } from './style/global';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const { GlobalStyle } = style();
 root.render(
-  <React.StrictMode>
+  <>
     <App />
     <GlobalStyle />
-  </React.StrictMode>
+  </>
 );
