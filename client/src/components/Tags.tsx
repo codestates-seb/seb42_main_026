546f39cf278235d0756a8b1b56645d5ae0fc16b6
jinaSE0 <tiffany6771@gmail.com>
2023-03-19 01:25:11 +0900

    feat:태그에 disable속성추가

--- client/src/components/Tags.tsx
+++ client/src/components/Tags.tsx
@@ -4,7 +4,6 @@ interface TagesProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title?: string;
   size?: "big" | "small";
   tagClickHandler?: React.MouseEventHandler<HTMLButtonElement>;
-
   disabled?: any;
 }
 
