e61cff06157ab2863ab38fef15c7170bf7b1f7ce
ssomni <ssm9991@gmail.com>
2023-03-19 02:11:36 +0900

    feat: 내가 쓴 글 조회 요청

--- client/src/page/MyPostsPage.tsx
+++ client/src/page/MyPostsPage.tsx
@@ -1,93 +1,89 @@
 import styled from "styled-components";
 import BoardItem from "../container/naggingboard/BoardItem";
+import useGetRank from "../hooks/useGetRank";
 
 const MyPostsPage = () => {
-  const dummyData = [
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "황금올리브닭다리",
-      likeCount: 220,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "고양킹",
-      likeCount: 100,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "강아지는귀여워",
-      likeCount: 660,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "자르반 4세",
-      likeCount: 603,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "피그마로 밥먹는 사람",
-      likeCount: 610,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "백엔드의 황제",
-      likeCount: 500,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "당신누구요",
-      likeCount: 620,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "연진아너X됐어",
-      likeCount: 699,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "리액트가뭔가요",
-      likeCount: 601,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-    {
-      title: "잔소리 오지게 해줄 사람",
-      nickname: "코드로 밥먹기",
-      likeCount: 654,
-      createdAt: "03/07",
-      answerCount: 20,
-    },
-  ];
+  // const dummyData = [
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "황금올리브닭다리",
+  //     likeCount: 220,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "고양킹",
+  //     likeCount: 100,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "강아지는귀여워",
+  //     likeCount: 660,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "자르반 4세",
+  //     likeCount: 603,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "피그마로 밥먹는 사람",
+  //     likeCount: 610,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "백엔드의 황제",
+  //     likeCount: 500,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "당신누구요",
+  //     likeCount: 620,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "연진아너X됐어",
+  //     likeCount: 699,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "리액트가뭔가요",
+  //     likeCount: 601,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  //   {
+  //     title: "잔소리 오지게 해줄 사람",
+  //     nickname: "코드로 밥먹기",
+  //     likeCount: 654,
+  //     createdAt: "03/07",
+  //     answerCount: 20,
+  //   },
+  // ];
+  const memberId = localStorage.getItem("memberId");
+  const data = useGetRank(`/members/${memberId}/questions`);
+  console.log(data);
+
   return (
     <MyPostsBoard>
-      {dummyData.map(
-        ({ title, nickname, likeCount, createdAt, answerCount }, index) => (
-          <BoardItem
-            key={index}
-            title={title}
-            likeCount={likeCount}
-            nickname={nickname}
-            createdAt={createdAt}
-            answerCount={answerCount}
-          />
-        )
-      )}
+      {data.map(({ title, nickname, likeCount, createdAt, answerCount }, index) => (
+        <BoardItem key={index} title={title} likeCount={likeCount} nickname={nickname} createdAt={createdAt} answerCount={answerCount} />
+      ))}
     </MyPostsBoard>
   );
 };
