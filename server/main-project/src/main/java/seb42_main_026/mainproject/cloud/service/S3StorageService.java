9c19d9a1c43dcef35ba54145b22dedb517005095
jbsim999 <116049579+jbsim999@users.noreply.github.com>
2023-03-20 15:09:44 +0900

    Refactor: 미디어파일 업로드 리팩토링 #6, #8, #15

--- server/main-project/src/main/java/seb42_main_026/mainproject/cloud/service/S3StorageService.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/cloud/service/S3StorageService.java
@@ -1,15 +1,19 @@
 package seb42_main_026.mainproject.cloud.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import seb42_main_026.mainproject.cloud.service.StorageService;
 import seb42_main_026.mainproject.exception.StorageException;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -22,17 +26,55 @@ public class S3StorageService implements StorageService {
     private String bucket;
 
     @Override
-    public void store(MultipartFile file){
+    public void store(MultipartFile file, String encodedFileName){
         try {
-            String fileName = file.getOriginalFilename();
-            String fileUrl = "https://" + bucket + "/answers" + fileName;
+//            String fileName = file.getOriginalFilename();
+
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentType(file.getContentType());
             metadata.setContentLength(file.getSize());
 
-            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+//            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+            //PutObjectRequest 이용하여 파일 생성 안하고 업로드
+            amazonS3Client.putObject(new PutObjectRequest(bucket, encodedFileName, file.getInputStream(), metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
         } catch (IOException e) {
             throw new StorageException("Failed to store file.", e);
         }
     }
+
+    //URL 생성 메서드 (파일명 한글일때, 깨짐 방지)
+    public String getFileUrl(String encodedFileName){
+        return URLDecoder.decode(amazonS3Client.getUrl(bucket,encodedFileName).toString(),StandardCharsets.UTF_8);
+    }
+
+    //파일명을 "UUID(랜덤값)-{fileName}" 으로 바꿔주는 메서드
+    public String encodeFileName(MultipartFile mediaFile){
+        String fileName = mediaFile.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+
+        return uuid + "-" + fileName;
+    }
+
+    //두가지 기능 합친거
+//    public String fileUrl(MultipartFile file){
+//        String fileName = encodeFileName(file);
+//
+//        try {
+//            ObjectMetadata metadata = new ObjectMetadata();
+//            metadata.setContentType(file.getContentType());
+//            metadata.setContentLength(file.getSize());
+//
+//            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
+//                    .withCannedAcl(CannedAccessControlList.PublicRead));
+//        } catch (IOException e) {
+//            throw new StorageException("Failed to store file.", e);
+//        }
+//
+//        String url = URLDecoder.decode(amazonS3Client.getUrl(bucket,fileName).toString(),StandardCharsets.UTF_8);
+//
+//        return url;
+//    }
+//
+//
 }
