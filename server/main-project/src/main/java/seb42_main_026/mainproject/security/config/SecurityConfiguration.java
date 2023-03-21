0346bffac6ab120c534750e0065689a0e8bc9b6e
NakChoi <nakchoi94@gmail.com>
2023-03-21 16:30:21 +0900

    Fix: cors 수정

--- server/main-project/src/main/java/seb42_main_026/mainproject/security/config/SecurityConfiguration.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/security/config/SecurityConfiguration.java
@@ -131,7 +131,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
 //        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 출처(Origin)에 대해 스크립트 기반의 HTTP 통신을 허용하도록 설정한다. 이 설정은 운영 서버 환경에서 요구사항에 맞게 변경이 가능하다.
 //        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE")); // 파라미터로 지정한 HTTP Method에 대한 HTTP 통신을 허용한다.
-        configuration.addAllowedOriginPattern("http://localhost:3000");
+        configuration.addAllowedOriginPattern("https://codestates-seb.github.io/seb42_main_026");
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("*"));
