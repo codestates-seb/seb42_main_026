3f6b0a242d3fb58c075c71afbfe5864c4f2807cc
jetty8013 <jetty8013@gmail.com>
2023-03-22 20:39:29 +0900

    feat : editorPage image 프리뷰 기능 추가

--- client/src/page/EditorPage.tsx
+++ client/src/page/EditorPage.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, ChangeEvent } from 'react';
 import styled from 'styled-components';
 import Tags from '../components/Tags';
 import { usePage } from '../hooks/usePage';
+import { ReactComponent as ICON_IMAGE } from '../assets/ic_editorpage_image_button.svg';
+import { ReactComponent as ICON_DELETE } from '../assets/ic_editorpage_delete_button.svg';
 
 const EditorPage = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [tag, setTag] = useState('ETC');
+  const [imgFile, setImgFile] = useState<File[]>([]);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const { getEditorHandler, setEditorHandler } = usePage();
 
   useEffect(() => {
@@ -30,6 +35,18 @@ const EditorPage = () => {
     }
   };
 
+  const previewImage = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setImgFile([...imgFile, event.target.files[0]]);
+    }
+  };
+
+  const deleteItem = (index: number) => {
+    const newImgFile = [...imgFile];
+    newImgFile.splice(index, 1);
+    setImgFile(newImgFile);
+  };
+
   return (
     <EditorPageWrapper>
       <TagSelector>
@@ -39,11 +56,33 @@ const EditorPage = () => {
         <Tags title={'기타'} size="small" tagClickHandler={() => setTag('ETC')} disabled={tag === 'ETC'} type="button" />
       </TagSelector>
       <TitleInput value={title} onChange={handleTitleChange} placeholder="제목"></TitleInput>
-      <ContentInput ref={textareaRef} rows={1} value={content} onChange={handleContentChange} placeholder="나에게 필요한 잔소리를 요청해보세요!" />
+
+      <ContentContainer>
+        <ContentInput ref={textareaRef} rows={1} value={content} onChange={handleContentChange} placeholder="나에게 필요한 잔소리를 요청해보세요!" />
+        <ImgContainer>
+          {imgFile.length > 0 &&
+            imgFile.map((file, index) => (
+              <ItemContainer key={index}>
+                <DeleteIcon>
+                  <ICON_DELETE onClick={() => deleteItem(index)} />
+                </DeleteIcon>
+                <ImgItem src={URL.createObjectURL(file)} alt="preview" />
+              </ItemContainer>
+            ))}
+        </ImgContainer>
+        <ImgAddButton onClick={() => inputRef.current?.click()}>
+          <ICON_IMAGE />
+        </ImgAddButton>
+      </ContentContainer>
+      <CustomInput ref={inputRef} type="file" accept="image/*" onChange={previewImage} />
     </EditorPageWrapper>
   );
 };
 export default EditorPage;
+
+const CustomInput = styled.input`
+  display: none;
+`;
 
 const EditorPageWrapper = styled.div`
   display: flex;
@@ -74,12 +113,57 @@ const TitleInput = styled.input`
   padding: 10px 0;
 `;
 
+const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
 const ContentInput = styled.textarea`
-  /* border: none; */
+  border: none;
   font-family: 'Noto Sans KR';
   font-size: var(--font-size16);
   font-weight: var(--font-weight400);
   letter-spacing: -0.025em;
   padding: 10px 0;
   resize: none;
+`;
+
+const ImgAddButton = styled.button`
+  width: 36px;
+  height: 36px;
+  border: none;
+  background-color: var(--color-white01);
+`;
+
+const DeleteIcon = styled.div`
+  top: 8px;
+  left: 8px;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  gap: 14px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const ImgItem = styled.img`
+  background-color: var(--color-gray01);
+  border-radius: 5px;
+  width: 48px;
+  height: 48px;
 `;
