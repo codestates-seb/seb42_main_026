9cb1605579fe3bad7724c45b32ad3c89db1a1e77
jiilEun <dmswldlf2010@gmail.com>
2023-03-23 17:58:35 +0900

    Fix: 인기글 정렬 기준 변경

--- server/main-project/src/main/java/seb42_main_026/mainproject/domain/question/service/QuestionService.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/domain/question/service/QuestionService.java
@@ -75,7 +75,7 @@ public class QuestionService {
     // 홈에서 인기 질문 목록 조회(좋아요 순, 동점일 때는 오래된 순, 10개만 조회)
     @Transactional(readOnly = true)
     public List<Question> findQuestionsAtHome() {
-        return questionRepository.findTop10ByOrderByLikeCountDescQuestionIdAsc();
+        return questionRepository.findTop10ByOrderByLikeCountDescAnswerCountDesc();
     }
 
     // 게시판에서 질문 목록 조회(최신 순, 페이지네이션)
