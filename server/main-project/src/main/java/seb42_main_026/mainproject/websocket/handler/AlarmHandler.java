63570b4ad2962b44a1906357a883119ed2d78a15
jiilEun <dmswldlf2010@gmail.com>
2023-03-23 11:41:21 +0900

    Fix: 변수명 수정

--- server/main-project/src/main/java/seb42_main_026/mainproject/websocket/handler/AlarmHandler.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/websocket/handler/AlarmHandler.java
@@ -8,27 +8,26 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class AlarmHandler extends TextWebSocketHandler {
-    private final ConcurrentHashMap<Long, WebSocketSession> memberSessions = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Long, WebSocketSession> sessions = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         Long memberId = (Long) session.getAttributes().get("memberId");
-        memberSessions.put(memberId, session);
+        sessions.put(memberId, session);
         session.sendMessage(new TextMessage("connected!"));
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         Long memberId = (Long) session.getAttributes().get("memberId");
-        memberSessions.remove(memberId);
+        sessions.remove(memberId);
     }
 
     public void sendAlarmToMember(Long memberId, String message) throws IOException {
-        WebSocketSession session = memberSessions.get(memberId);
+        WebSocketSession session = sessions.get(memberId);
 
         if (session != null && session.isOpen()) {
             session.sendMessage(new TextMessage(message));
