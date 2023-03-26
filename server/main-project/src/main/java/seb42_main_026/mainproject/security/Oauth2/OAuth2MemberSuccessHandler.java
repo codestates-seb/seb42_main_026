df3739e7ef4406565d330d923332419198c546a8
NakChoi <nakchoi94@gmail.com>
2023-03-26 12:42:43 +0900

    Fix: config 수정

--- server/main-project/src/main/java/seb42_main_026/mainproject/security/Oauth2/OAuth2MemberSuccessHandler.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/security/Oauth2/OAuth2MemberSuccessHandler.java
@@ -100,10 +100,10 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
         return UriComponentsBuilder //  Port 설정을 하지 않으면 기본값은 80 포트
                 .newInstance()
                 .scheme("http")
-                //.host("seb42-main-026-fe.s3-website.ap-northeast-2.amazonaws.com") // 서버용
-                //.path("/login/callback") // 서버용
-                .host("localhost")
-                .path("receive-token.html")
+                .host("seb42-main-026-fe.s3-website.ap-northeast-2.amazonaws.com") // 서버용
+                .path("/login/callback") // 서버용
+                //.host("localhost")
+                //.path("receive-token.html")
                 .queryParams(queryParams)
                 .build()
                 .toUri();
