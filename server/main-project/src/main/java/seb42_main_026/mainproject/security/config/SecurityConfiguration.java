399b1db15141b0263955ca1c98d4211cbe4437ce
NakChoi <nakchoi94@gmail.com>
2023-03-26 12:02:07 +0900

    Fix: SecurityConfig 수정

--- server/main-project/src/main/java/seb42_main_026/mainproject/security/config/SecurityConfiguration.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/security/config/SecurityConfiguration.java
@@ -52,10 +52,6 @@ public class SecurityConfiguration {
     @Value("${spring.security.oauth2.client.registration.google.client-secret}")
     private String googleClientSecret;
 
-    /*private String googleClientId= "643952365035-kagaao62of75uvvq3dml1468mpg5hr9v.apps.googleusercontent.com";
-
-    private String googleClientSecret= "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";*/
-
     @Value("${spring.security.oauth2.client.registration.naver.client-id}")
     private String naverClientId;
     @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
@@ -114,8 +110,6 @@ public class SecurityConfiguration {
                         .anyRequest().permitAll()//authenticated()              // JWT를 적용하기 전이므로 우선은 모든 HTTP request 요청에 대해서 접근을 허용하도록 설정했다.
 
                 )
-               /* .oauth2Login(oauth2 -> oauth2
-                        .successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberRepository)));*/ // OAuth 2 로그인 인증을 활성화한다.
                 .oauth2Login()
                     .userInfoEndpoint()
                         .userService(customOAuth2UserService)
@@ -141,11 +135,6 @@ public class SecurityConfiguration {
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
-/*=======
-        //configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 출처(Origin)에 대해 스크립트 기반의 HTTP 통신을 허용하도록 설정한다. 이 설정은 운영 서버 환경에서 요구사항에 맞게 변경이 가능하다.
-        configuration.addAllowedOriginPattern("http://localhost:3000");
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE")); // 파라미터로 지정한 HTTP Method에 대한 HTTP 통신을 허용한다.
->>>>>>> Stashed changes*/
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // CorsConfigurationSource 인터페이스의 구현 클래스인 UrlBasedCorsConfigurationSource 클래스의 객체를 생성한다.
         source.registerCorsConfiguration("/**", configuration);         // 모든 URL에 앞에서 구성한 CORS 정책(CorsConfiguration)을 적용한다.
