6ab617d207d9d7e234b528a64c8dc5c15e6a9a7c
jetty8013 <jetty8013@gmail.com>
2023-03-27 17:23:09 +0900

    feat : CustomAudio CommentForm에 반영

--- client/src/container/postdetail/CommentForm.tsx
+++ client/src/container/postdetail/CommentForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { answer, addComment } from '../../api/answer';
+import CustomAudio from '../../components/CustomAudio';
 // import ICON_SUBIT from '../../assets/ic_commentform_submit_button.svg';
 
 type Props = {
@@ -10,37 +11,7 @@ type Props = {
 
 const CommentForm: React.FC<Props> = ({ questionId, answerId }) => {
   const [comment, setComment] = useState<string>('');
-  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
-  const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const mediaRecorder = new MediaRecorder(stream);
-
-      mediaRecorder.addEventListener('dataavailable', (event: BlobEvent) => {
-        if (event.data.size > 0) {
-          setAudioChunks((chunks) => [...chunks, event.data]);
-        }
-      });
-
-      setRecorder(mediaRecorder);
-      mediaRecorder.start();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const stopRecording = () => {
-    if (recorder) {
-      recorder.stop();
-      setRecorder(null);
-    }
-  };
-
-  const clearRecording = () => {
-    setAudioChunks([]);
-  };
 
   const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setComment(event.target.value);
@@ -48,7 +19,7 @@ const CommentForm: React.FC<Props> = ({ questionId, answerId }) => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+    // const audioBlob = new Blob(chunksRef.current, { type: 'audio/webm' });
     if (questionId !== undefined && answerId === undefined) {
       answer(comment, questionId);
     } else if (answerId !== undefined && questionId !== undefined) {
@@ -56,38 +27,21 @@ const CommentForm: React.FC<Props> = ({ questionId, answerId }) => {
     }
     // onSubmit(comment, audioBlob);
     setComment('');
-    setAudioChunks([]);
+    // chunksRef.current = [];
   };
-
-  useEffect(() => {
-    const audioElement = document.querySelector('audio');
-    if (audioElement) {
-      audioElement.srcObject = null;
-      audioElement.src = URL.createObjectURL(new Blob(audioChunks, { type: 'audio/webm' }));
-    }
-  }, [audioChunks]);
 
   return (
     <>
       <CommentInputWrapper>
         <form onSubmit={handleSubmit}>
           <CommentInput placeholder="당신의 잔소리가 필요해요!" value={comment} onChange={handleCommentChange} />
-          <NoneButton ref={buttonRef} type="submit">
-            댓글 작성
-          </NoneButton>
+          <ButtonWrapper>
+            <CustomAudio />
+            <CommentButton type="submit" onClick={() => buttonRef.current?.click()}>
+              댓글 작성
+            </CommentButton>
+          </ButtonWrapper>
         </form>
-        <ButtonWrapper>
-          {/* <AudioButtonContainer>
-              {!recorder ? <button onClick={startRecording}>녹음시작</button> : <button onClick={stopRecording}>녹음정지</button>}
-              <button onClick={clearRecording}>초기화</button>
-              <CustomAudio controls>
-                {audioChunks.map((chunk, i) => (
-                  <source key={i} src={URL.createObjectURL(chunk)} />
-                ))}
-              </CustomAudio>
-            </AudioButtonContainer> */}
-          <CommentButton onClick={() => buttonRef.current?.click()}>➜</CommentButton>
-        </ButtonWrapper>
       </CommentInputWrapper>
     </>
   );
@@ -95,14 +49,6 @@ const CommentForm: React.FC<Props> = ({ questionId, answerId }) => {
 
 export default CommentForm;
 
-const NoneButton = styled.button`
-  display: none;
-`;
-
-const CustomAudio = styled.audio`
-  width: 96px;
-  height: 48px;
-`;
 
 const CommentInputWrapper = styled.div`
   width: 100%;
@@ -120,7 +66,7 @@ const CommentInput = styled.textarea`
   border-radius: 5px;
   padding: 10px;
   overflow-y: hidden;
-  font-size: 16px;
+  font-size: 14px;
   resize: none;
   height: 60px;
   margin-bottom: 10px;
@@ -128,33 +74,23 @@ const CommentInput = styled.textarea`
   border: none;
   :focus {
     outline: none;
-    font-family: 'Noto Sans KR';
   }
 `;
 
 const CommentButton = styled.button`
   background-color: var(--color-mobMain);
+  white-space: nowrap;
   color: #fff;
   border: none;
-  font-size: 16px;
-  padding: 13px 15px;
-  margin: 5px 10px;
-  border-radius: 50%;
-  transform: rotate(-90deg);
+  font-size: 14px;
+  padding: 16px;
   cursor: pointer;
 `;
 
 const ButtonWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
   border-top: solid 0.5px var(--color-gray01);
-`;
-
-const AudioButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
 `;
