03d8a60dce6892a291e140271c1c08eb02589ad0
NakChoi <nakchoi94@gmail.com>
2023-03-30 14:36:44 +0900

    Fix: Oauth2 Failure Handler 수정

--- server/main-project/src/main/java/seb42_main_026/mainproject/exception/ExceptionCode.java
+++ server/main-project/src/main/java/seb42_main_026/mainproject/exception/ExceptionCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionCode {
 
 
-    MEMBER_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다.", "email"),
+    MEMBER_EXISTS(HttpStatus.CONFLICT, "MEMBER_EXIST.", "email"),
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보가 없습니다.", null),
 
