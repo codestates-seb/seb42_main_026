ec404c56c84a4053e33e91664617aa39a1e4422d
jetty8013 <jetty8013@gmail.com>
2023-03-31 17:30:19 +0900

    mypage loading 애니메이션 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,14 +1,17 @@
 import { createStore, combineReducers } from 'redux';
+import appReducer from './reducers/appReducer';
 import authReducer from './reducers/authReducer';
 import pageReducer from './reducers/editorReducer';
 import postDetailReducer from './reducers/postDetailReducer';
 import userInfoReducer from './reducers/userInfoReducer';
+
 
 const rootReducer = combineReducers({
   auth: authReducer,
   page: pageReducer,
   post: postDetailReducer,
   user: userInfoReducer,
+  isLoading: appReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
