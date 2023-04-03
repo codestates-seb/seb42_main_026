d089cf9fd133b116f88b41d1270ce7f9ef906073
jetty8013 <jetty8013@gmail.com>
2023-04-03 13:15:57 +0900

    fix

--- client/src/store/reducers/authReducer.ts
+++ client/src/store/reducers/authReducer.ts
@@ -1,29 +1,8 @@
 import getCookie from '../../utils/cookieUtils';
-import decodeJwt from '../../utils/jwtUtils';
 import { LOGIN, LOGOUT } from '../constants';
 
 const accessToken = getCookie('accessToken');
 const refreshToken = getCookie('refreshToken');
-
-function deleteCookie(name: string, path: string = '/') {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:01 GMT; path=' + path;
-}
-
-
-const now = new Date();
-
-const decoded1 = decodeJwt(accessToken);
-const expiresAt1 = new Date(decoded1.exp * 1000);
-if (expiresAt1 < now) {
-  deleteCookie('accessToken');
-}
-
-const decoded2 = decodeJwt(refreshToken);
-const expiresAt2 = new Date(decoded2.exp * 1000);
-if (expiresAt2 < now) {
-  deleteCookie('refreshToken');
-}
-
 
 
 export interface AuthState {
