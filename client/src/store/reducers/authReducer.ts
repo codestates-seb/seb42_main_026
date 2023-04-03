a99cf5d1b3e14726226f4b2703c66de3e0767cf6
jetty8013 <jetty8013@gmail.com>
2023-04-03 13:18:46 +0900

    fix

--- client/src/store/reducers/authReducer.ts
+++ client/src/store/reducers/authReducer.ts
@@ -1,5 +1,25 @@
 import getCookie from '../../utils/cookieUtils';
+import decodeJwt from '../../utils/jwtUtils';
 import { LOGIN, LOGOUT } from '../constants';
+
+
+function deleteExpiredCookies() {
+  const cookies = document.cookie.split(';');
+  const now = new Date();
+
+  cookies.forEach((cookie) => {
+    const [name, value] = cookie.split('=');
+    if (name.trim() === 'accessToken' || name.trim() === 'refreshToken') {
+      const decoded = decodeJwt(value);
+      const expiresAt = new Date(decoded.exp * 1000);
+      if (expiresAt < now) {
+        document.cookie = `${name.trim()}=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+      }
+    }
+  });
+}
+
+deleteExpiredCookies();
 
 const accessToken = getCookie('accessToken');
 const refreshToken = getCookie('refreshToken');
